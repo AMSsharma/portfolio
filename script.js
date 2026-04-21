05a48ce40e2ec0769ecd0b9a02613dcab2bca456
Aditya <thelegendmsd7@gmail.com>
2026-04-21 17:49:08 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll(".project-image");
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("fullImage");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+  });
+});
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+modal.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
